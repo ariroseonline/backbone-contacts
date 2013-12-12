897ac6ea686a88ac5b4cf52cdb5c08e8701203e4
ariroseonline <ariroseonline@gmail.com>
2013-12-11 20:47:11 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackboneContacts::Application.config.secret_key_base = 'c1c9975862c4cd6676e607bc9cba1eb49e127b7c7c77b7fcd46092e556e51f949f07015b5fac35ab4524288261c045c954fadb8fb3b640ce548dd329a3aa8a55'
